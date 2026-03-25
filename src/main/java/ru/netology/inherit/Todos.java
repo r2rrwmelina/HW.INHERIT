008c8a6550511046ea2b61ed8bceb8466358d510
MG <gmelina69@gmail.com>
2026-03-25 14:28:56 +0400

    Добавить класс-менеджер Todos для хранения задач
    1. Создание массива Task[] для хранения задач в общем массиве, что упрощает и сокращает код менеджера

---
+++ src/main/java/ru/netology/inherit/Todos.java
@@ -0,0 +1,22 @@
+package ru.netology.inherit;
+
+public class Todos {
+    private Task[] tasks = new Task[0];
+
+    public Task[] addToArray(Task[] current, Task task) {
+        Task[] tmp = new Task[current.length + 1];
+        for (int i = 0; i < current.length; i++) {
+            tmp[i] = current[i];
+        }
+        tmp[tmp.length - 1] = task;
+        return tmp;
+    }
+
+    public void add(Task task) {
+        tasks = addToArray(tasks, task);
+    }
+
+    public Task[] findAll() {
+        return tasks;
+    }
+}
